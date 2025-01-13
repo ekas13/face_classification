b365a7bfe181f2ec25fb542e08a681e382e45e67
nandortakacs <nandortakacs123@gmail.com>
2025-01-13 16:48:23 +0100

    basic training and loop, testing, and model

--- src/face_classification/evaluate.py
+++ src/face_classification/evaluate.py
@@ -5,8 +5,16 @@ from torch.utils.data import DataLoader
 from torchvision.transforms import ToTensor
 import typer
 from model import PretrainedResNet34
+import os
+from typing import Union
 
-def evaluate(model_checkpoint: str) -> None:
+# Define a specific checkpoint from the checkpoints directory
+model_checkpoint: str = os.path.join(
+    os.path.dirname(__file__), "..", "..", "checkpoints", "model-epoch=01-val_loss=1.68.ckpt"
+    )
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+def evaluate(model_checkpoint: str = model_checkpoint) -> None:
     """Evaluate a trained model using PyTorch Lightning Trainer."""
     print("Evaluating with model checkpoint:", model_checkpoint)
 
@@ -15,10 +23,11 @@ def evaluate(model_checkpoint: str) -> None:
     test_dataset = torchvision.datasets.CIFAR10(
         root="../../data", train=False, download=True, transform=ToTensor()
     )
-    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+    test_dataloader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
 
     model = PretrainedResNet34(num_classes=10)
-    checkpoint = torch.load(model_checkpoint)
+    checkpoint = torch.load(model_checkpoint, map_location=device)
+    model.to(device)
     model.load_state_dict(checkpoint["state_dict"])
 
     # Initialize the PyTorch Lightning trainer
