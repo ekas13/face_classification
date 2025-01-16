389daba00ed7b420683314218a7ccd5e86c80465
Nandor Takacs <nandortakacs123@gmail.com>
2025-01-16 17:41:56 +0100

    profiler

--- src/face_classification/train.py
+++ src/face_classification/train.py
@@ -2,6 +2,7 @@ import torch
 import typer
 from pytorch_lightning import Trainer
 from pytorch_lightning.callbacks import ModelCheckpoint
+from torch.profiler import profile, ProfilerActivity, tensorboard_trace_handler
 
 from metric_tracker import MetricTracker
 from model import PretrainedResNet34
@@ -11,12 +12,22 @@ app = typer.Typer()
 
 # Hyperparameters
 BATCH_SIZE = 32
-EPOCHS = 30
-
+EPOCHS = 10
+USE_PROFILER = True
 
 @app.command()
 def train(batch_size: int = BATCH_SIZE, epochs: int = EPOCHS) -> None:
-    """Train a model on CIFAR10."""
+    if USE_PROFILER:
+        with profile(activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA], record_shapes=True, profile_memory=True, on_trace_ready=tensorboard_trace_handler("./logs/PretrainedResNet34")) as prof:
+            run_training(batch_size, epochs)
+            # print(prof.key_averages().table(sort_by="cpu_time_total", row_limit=10))
+            # print(prof.key_averages(group_by_input_shape=True).table(sort_by="cpu_time_total", row_limit=30))
+            prof.export_chrome_trace("./logs/train_trace.json")
+    else:
+        run_training(batch_size, epochs)
+
+def run_training(batch_size: int, epochs: int) -> None:
+    """Train our model on the Face Dataset."""
     print("Training day and night")
 
     train_set = FaceDataset(mode="train")
@@ -37,7 +48,6 @@ def train(batch_size: int = BATCH_SIZE, epochs: int = EPOCHS) -> None:
     trainer = Trainer(max_epochs=epochs, callbacks=[checkpoint_callback, metric_tracker], accelerator="auto")
     trainer.fit(model, train_dataloader, val_dataloaders=val_dataloader)
     print("Training finished")
-
 
 if __name__ == "__main__":
     typer.run(train)
