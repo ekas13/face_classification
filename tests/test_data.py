371017b257669a3c440c77576b3d41845a3bdc9a
biabragac <147725126+biabragac@users.noreply.github.com>
2025-01-21 17:29:51 +0000

    Pre-commit fixes

--- tests/test_data.py
+++ tests/test_data.py
@@ -2,8 +2,9 @@ import os.path
 
 import pytest
 import torch
-from face_classification.data import FaceDataset
 from torch.utils.data import Dataset
+
+from face_classification.data import FaceDataset
 
 
 @pytest.mark.skipif(not os.path.exists("data/processed"), reason="Data files not found")
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,7 +1,8 @@
 import hydra
 import torch
-from face_classification.model import PretrainedResNet34
 from pytorch_lightning import Trainer
+
+from face_classification.model import PretrainedResNet34
 
 with hydra.initialize(config_path="../configs", version_base=None, job_name="train_model"):
     config = hydra.compose(config_name="default_config")
