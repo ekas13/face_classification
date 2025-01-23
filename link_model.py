d7a9c9f1e2b2a7713a2860a3b245797d19a813b4
biabragac <s233576@dtu.dk>
2025-01-23 21:47:26 +0100

    update link model input and model registry url

--- link_model.py
+++ link_model.py
@@ -2,38 +2,39 @@ import os
 import urllib.parse
 
 import typer
+from omegaconf import OmegaConf
 
 import wandb
 
 
-def link_model(artifact_path: str, aliases: list[str] = ["staging"]) -> None:
+def link_model(version: str, aliases: list[str] = ["staging"]) -> None:
     """
     Stage a specific model to the model registry.
 
     Args:
-        artifact_path: Path to the artifact to stage.
-            Should be of the format "entity/project/artifact_name:version".
+        version: Version of the artifact to stage (e.g., "v8").
         aliases: List of aliases to link the artifact with.
 
     Example:
-        model_management link-model entity/project/artifact_name:version -a staging -a best
+        model_management link-model v8 -a staging -a best
 
     """
-    if artifact_path == "":
-        typer.echo("No artifact path provided. Exiting.")
+    config = OmegaConf.load("configs/default_config.yaml")
+
+    if version == "":
+        typer.echo("No artifact version provided. Exiting.")
         return
+
+    model_registry_url = config.urls.wandb_registry
+    artifact_path = f"{model_registry_url}:{version}"
 
     api = wandb.Api(
         api_key=os.getenv("WANDB_API_KEY"),
         overrides={"entity": os.getenv("WANDB_ENTITY"), "project": os.getenv("WANDB_PROJECT")},
     )
-    _, _, artifact_name_version = artifact_path.split("/")
 
     artifact = api.artifact(artifact_path)
-    artifact.link(
-        target_path=f"vbranica-danmarks-tekniske-universitet-dtu-org/wandb-registry-face_classification_registry/Model_collection:latest",
-        aliases=aliases,
-    )
+    artifact.link(target_path=model_registry_url, aliases=aliases)
     artifact.save()
     typer.echo(f"Artifact {artifact_path} linked to {aliases}")
 
