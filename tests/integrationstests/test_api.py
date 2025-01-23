1fc39d3b12430f15411787e62ebbce801573660f
Vilim <vbranica@gmail.com>
2025-01-23 15:19:17 +0100

    Changed test structure

--- tests/integrationstests/test_api.py
+++ tests/integrationstests/test_api.py
@@ -1,19 +1,1 @@
-from fastapi.testclient import TestClient
-from web_app.api import app
 
-client = TestClient(app)
-
-def test_train_api(model):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the MNIST model inference API!"}
-
-def test_evaluate_api(model):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the MNIST model inference API!"}
-
-def test_predict(model):
-    response = client.get("/")
-    assert response.status_code == 200
-    assert response.json() == {"message": "Welcome to the MNIST model inference API!"}
