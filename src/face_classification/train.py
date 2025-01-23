76b23a811f60764179a4c897f3f9d84d7c33b73d
vilim-cro <79641968+vilim-cro@users.noreply.github.com>
2025-01-23 15:14:06 +0000

    Pre-commit fixes

--- src/face_classification/train.py
+++ src/face_classification/train.py
@@ -4,6 +4,7 @@ import os
 import hydra
 import torch
 import typer
+import wandb
 from google.cloud import storage
 from omegaconf import OmegaConf
 from pytorch_lightning import Trainer
@@ -11,7 +12,6 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 from pytorch_lightning.loggers import WandbLogger
 from torch.profiler import ProfilerActivity, profile, tensorboard_trace_handler
 
-import wandb
 from face_classification.data import FaceDataset
 from face_classification.metric_tracker import MetricTracker
 from face_classification.model import PretrainedResNet34
